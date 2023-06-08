f4fcf99e73ccf1dbfe49893bc0e14a61c6b1e09a
Alex <alexander.krivorotko@gmail.com>
2023-06-08 14:54:31 +0700

    update

--- tests/app-feature.test.js
+++ tests/app-feature.test.js
@@ -2,12 +2,16 @@ const { parseVersion } = require('../utils');
 const appVersion = parseVersion(process.env.APP_VERSION);
 
 describe('App suite @app', function () {
+	it('old test any version', async function () {});
+
 	if (appVersion <= parseVersion('2.8'))
-		it('app test 1 <=2.8', async function () {});
+		it('update statistics <=2.8', async function () {});
 
 	if (appVersion <= parseVersion('2.9'))
-		it('app test 1 <=2.9', async function () {});
+		it('alerter test <=2.9', async function () {});
 
-	it('app test 2 any', async function () {});
-	it('app test 3 any', async function () {});
+	if (appVersion >= parseVersion('2.10')) {
+		it('new test 1 >=2.10', async function () {});
+		it('new test 2 >=2.10', async function () {});
+	}
 });
